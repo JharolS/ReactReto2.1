ca60d7b93fb91edb71b9a2bc4408467d6d05abdf
unknown <sv73107535@idat.edu.pe>
2021-10-05 17:32:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="row1">
+        <div className="city1">
+            <div className="letter">travel</div>
+            <img 
+            style={{width: 66, height: 58, marginLeft: 3, marginTop: 3, opacity: 0.1}}
+            alt=""src="https://w7.pngwing.com/pngs/254/638/png-transparent-moscow-architecture-sticker-castle-building-world-mosque.png"/>
+            <div className="name">Moscow</div>
+        </div>      
+        <div className="city2">
+            <div className="letter">travel</div>
+            <img 
+            style={{width: 66, height: 58, marginLeft: 3, marginTop: 3, opacity: 0.1}}
+            alt=""src="https://ae01.alicdn.com/kf/HTB1CPQSw2iSBuNkSnhJq6zDcpXaD/Estambul-pared-calcoman-a-pegatina-de-vinilo-pegatinas-decoraci-n-arte-Mural-sala-de-Decoraci-n.jpg_q50.jpg"/>
+            <div className="name">Istanbul</div>
+        </div>
+        </div>
+        <div className="row2">
+        <div className="city3">
+            <div className="letter">travel</div>
+            <img
+            style={{width: 66, height: 58, marginLeft: 3, marginTop: 3, opacity: 0.1}}
+            alt=""src="https://img2.freepng.es/20180329/hfq/kisspng-colosseum-eur-rome-eiffel-tower-sticker-colosseum-5abc76fa425d20.9645857615223006662718.jpg"/>
+            <div className="name">Rome</div>
+        </div>  
+        <div className="city4">
+            <div className="letter">travel</div>
+            <img 
+            style={{width: 66, height: 58, marginLeft: 3, marginTop: 3, opacity: 0.1}} 
+            alt=""src="https://i.pinimg.com/474x/92/4d/2e/924d2e171e5088d5de240a76650c06dc.jpg"/>
+            <div className="name">Beijing</div>
+        </div>
+        </div>
     </div>
   );
 }
